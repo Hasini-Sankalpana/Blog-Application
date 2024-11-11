6bdfbf4a8113be90dc27024c487870f55dfde016
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-11-11 11:29:43 +0530

    Connect to the database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
+import { config } from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then (() => {
+    console.log('MongoDB is connected !');
+})
+.catch((err) => {
+    console.log(err)
+});
+
 
 const app = express ();
 
